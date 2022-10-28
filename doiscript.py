41b2bd550f5b4e9316b233bfa1c09e0e517a1152
barba <alvaro.jeria@uv.cl>
2022-10-27 21:15:57 -0300

    Ver 2.0

--- doiscript.py
+++ doiscript.py
@@ -27,95 +27,45 @@ samplebib = """
 }
 """
 
-def bibparser(bibtext):
-    bibdict = {}
-    bibtext = bibtext.replace("{","").replace("}","").strip().split(",")
-    bibdict = dict(list(map(lambda x:x.strip().split(" = "),bibtext))[1:])
-    return bibdict
+
 
 def journalsearch(journalname):
-    match = dl.get_close_matches(journalname,JOURNALS["Journal name"])
-    print("Matching",journalname,end=" ")
+    match = dl.get_close_matches(journalname, JOURNALS["Journal name"])
+    print("Matching", journalname, end=" ")
     if match:
-        candidateRow = JOURNALS[JOURNALS["Journal name"]==match[0]]
-        print("->",candidateRow["Journal name"].values[0])
+        candidateRow = JOURNALS[JOURNALS["Journal name"] == match[0]]
+        print("->", candidateRow["Journal name"].values[0])
         if candidateRow['ISSN'].values[0] is np.NAN:
-            return candidateRow["eISSN"].values[0],round(candidateRow["2021 JIF"].values[0],3), candidateRow['JIF Quartile'].values[0]
+            return candidateRow["eISSN"].values[0], round(candidateRow["2021 JIF"].values[0], 3), \
+                   candidateRow['JIF Quartile'].values[0]
         else:
-            return candidateRow["ISSN"].values[0],round(candidateRow["2021 JIF"].values[0],3), candidateRow['JIF Quartile'].values[0]
+            return candidateRow["ISSN"].values[0], round(candidateRow["2021 JIF"].values[0], 3), \
+                   candidateRow['JIF Quartile'].values[0]
     print("no match")
-    return "X-X",0.0, 'n/a'
+    return "X-X", 0.0, 'n/a'
 
-def doitobib(doi="10.1126/science.169.3946.635"):
-    if "doi.org" in doi:
-        doi = doi.split(".org/")[1]
-    url = BASE_URL + doi
-    print(url,end="")
-    req = urllib.request.Request(url)
-    req.add_header('Accept', 'application/x-bibtex')
-    try:
-        with urllib.request.urlopen(req, timeout = 1) as f:
-            bibtex = urllib.parse.unquote_plus(f.read().decode("utf-8"), encoding='utf-8', errors='replace')
-        print(" - OK!")
-        return bibtex
-    except:
-        print(" - Error!")
-        return ""
+# script
 
-def doitotxt(doi="10.1126/science.169.3946.635"):
-    if "doi.org" in doi:
-        doi = doi.split(".org/")[1]
-    url = BASE_URL + doi
-    print(url,end=" TXT ")
-    req = urllib.request.Request(url)
-    req.add_header('Accept', 'text/x-bibliography')
-    try:
-        with urllib.request.urlopen(req, timeout = 1) as f:
-            txt = f.read()#.decode("utf-8")#urllib.parse.unquote_plus(, encoding='utf-8', errors='replace')
-        print(" - OK!")
-        return txt
-    except:
-        print(" - Error!")
-        return ""
-
-def doitojson(doi="10.3389/fncel.2017.00174"):
-    if "doi.org" in doi:
-        doi = doi.split(".org/")[1]
-    url = BASE_URL + doi
-    print(url,end=" JSON ")
-    req = urllib.request.Request(url)
-    req.add_header('Accept', 'application/json')
-    try:
-        with urllib.request.urlopen(req, timeout = 1) as f:
-            json = loads(f.read().decode("utf-8"))#urllib.parse.unquote_plus(f.read().decode("utf-8"), encoding='utf-8', errors='replace'))
-            newjson={"authors":json["author"],"title":json["title"],"volume":json["volume"],"issn":json["ISSN"],"year":json["indexed"]["date-parts"],"journal":json["container-title"],}
-        print(" - OK!")
-        return newjson
-    except:
-        print(" - Error!")
-        return {}
-
-#script
-
-if len(sys.argv)>1:
+if len(sys.argv) > 1:
     doifile = sys.argv[1]
 else:
-    doifile = "KarenCastillo.doi"
-bibfile = doifile.split(".")[0]+".txt"
+    doifile = "PatricioOrio.doi"
+bibfile = doifile.split(".")[0] + ".txt"
 outbibs = []
 with open(doifile) as dois:
     for line in dois:
-        #print(doitojson(line.strip()),doitotxt(line.strip()))
+        # print(doitojson(line.strip()),doitotxt(line.strip()))
         # bib = doitojson(line.strip())
         if "doi.org" in line:
             line = line.split(".org/")[1]
         url = BASE_URL + line
-        print(url,end=" JSON ")
+        print(url, end=" JSON ")
         req = urllib.request.Request(url)
         req.add_header('Accept', 'application/json')
         try:
-            with urllib.request.urlopen(req, timeout = 5) as f:
-                json = loads(f.read().decode("utf-8"))#urllib.parse.unquote_plus(f.read().decode("utf-8"), encoding='utf-8', errors='replace'))
+            with urllib.request.urlopen(req, timeout=5) as f:
+                json = loads(f.read().decode(
+                    "utf-8"))  # urllib.parse.unquote_plus(f.read().decode("utf-8"), encoding='utf-8', errors='replace'))
             authlist = []
             for nn in json['author']:
                 try:
@@ -123,43 +73,44 @@ with open(doifile) as dois:
                 except:
                     authlist.append(f"{nn['family']}")
             authorsF = ', '.join(authlist)
-            vol=""
+            vol = ""
             if 'volume' in json.keys():
                 vol = json["volume"]
             if 'page' in json.keys():
-                vol = vol+':'+json["page"]
+                vol = vol + ':' + json["page"]
             elif 'issue' in json.keys():
-                vol = vol+'('+json["issue"]+')'
-            newjson={"authors":authorsF,"title":json["title"],"volume":vol,"issn":json["ISSN"],"year":json['published']['date-parts'],"journal":json["container-title"],}
+                vol = vol + '(' + json["issue"] + ')'
+            newjson = {"authors": authorsF, "title": json["title"], "volume": vol, "issn": json["ISSN"],
+                       "year": json['published']['date-parts'], "journal": json["container-title"], }
             while '  ' in newjson['title']:
-                newjson['title'] = newjson['title'].replace('  ',' ')
-            newjson['title'] = newjson['title'].replace('\n ','')
-            newjson['title'] = newjson['title'].replace('<sup>','')
-            newjson['title'] = newjson['title'].replace('</sup>','')
-            newjson['title'] = newjson['title'].replace('<sub>','')
-            newjson['title'] = newjson['title'].replace('</sub>','')
+                newjson['title'] = newjson['title'].replace('  ', ' ')
+            newjson['title'] = newjson['title'].replace('\n ', '')
+            newjson['title'] = newjson['title'].replace('<sup>', '')
+            newjson['title'] = newjson['title'].replace('</sup>', '')
+            newjson['title'] = newjson['title'].replace('<sub>', '')
+            newjson['title'] = newjson['title'].replace('</sub>', '')
 
             print(" - OK!")
-            bib =  newjson
+            bib = newjson
         except:
             print(" - Error!")
-            bib= {}
-        
-        
+            bib = {}
+
         if bib:
-            #print(bib)
-            #bibdict = bibparser(bib)
+            # print(bib)
+            # bibdict = bibparser(bib)
             # autores = ", ".join([a["given"]+" "+a["family"] for a in bib["authors"]])
-            issn,impact, Q = journalsearch(bib["journal"].upper())
-            out="\t".join([bib['authors'],str(bib["year"][0][0]),bib['title'],bib["journal"]+", "+bib["volume"]+" "+BASE_URL+line.strip(),"Publicada",str(issn),f'{impact:.3f} ({Q})'])+"\n"
-            out=out.replace('\u2010','-')
-            
+            issn, impact, Q = journalsearch(bib["journal"].upper())
+            out = "\t".join([bib['authors'], str(bib["year"][0][0]), bib['title'],
+                             bib["journal"] + ", " + bib["volume"] + " " + BASE_URL + line.strip(), "Publicada",
+                             str(issn), f'{impact:.3f} ({Q})']) + "\n"
+            out = out.replace('\u2010', '-')
+
             outbibs.append(out)
         else:
             print(bib, "No encontrado")
         print()
-    
 
-with open('../' + bibfile,"w", encoding="utf-8") as wbib:
+with open('./' + bibfile, "w", encoding="utf-8") as wbib:
     for b in outbibs:
         wbib.write(b)
